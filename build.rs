335e0abae46ba48bdbad6d4c7ea8b87af3801b0f
Tomio <mail@tomio.codes>
2022-03-18 20:09:12 +0800

    Update build.rs

--- build.rs
+++ build.rs
@@ -1,5 +1,13 @@
 extern crate napi_build;
 
 fn main() {
+	if cfg!(target_env = "musl") {
+		let _ = std::process::Command::new("sh")
+			.arg("-c")
+			.arg("cp /usr/lib/libsodium.so.23 target/release/deps")
+			.output()
+			.unwrap();
+	}
+	
     napi_build::setup();
 }
