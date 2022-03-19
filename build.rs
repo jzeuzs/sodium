a3383e662652c4212e3cbb842b176c66be85307c
Tomio <mail@tomio.codes>
2022-03-19 22:47:37 +0800

    fix(ci): release 0.0.1

--- build.rs
+++ build.rs
@@ -4,10 +4,10 @@ fn main() {
 	if cfg!(target_env = "musl") {
 		let _ = std::process::Command::new("sh")
 			.arg("-c")
-			.arg("cp /usr/lib/libsodium.so.23 target/release/deps")
+			.arg("cp /usr/lib/libsodium.so target/release/deps")
 			.output()
 			.unwrap();
 	}
-	
+
     napi_build::setup();
 }
