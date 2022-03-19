4cef7a624108fcd20c3a52a214e797d7bae8cb11
Tomio <mail@tomio.codes>
2022-03-20 01:38:07 +0800

    fix(deps): update sodiumoxide (0.0.1)

--- build.rs
+++ build.rs
@@ -1,6 +1,7 @@
 extern crate napi_build;
 
 fn main() {
+        // doing this manually because of issues in musl
 	if cfg!(target_env = "musl") {
 		let _ = std::process::Command::new("sh")
 			.arg("-c")
