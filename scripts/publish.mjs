29146a615afe3e13b3f295b8d8a09fedaae9bccb
Tomio <mail@tomio.codes>
2022-03-24 08:26:51 +0800

    feat: release 0.0.1

--- scripts/publish.mjs
+++ scripts/publish.mjs
@@ -1,12 +1,14 @@
-import git from 'git-last-commit';
 import { promisify } from 'node:util';
 import semverRegex from 'semver-regex';
 import publish from '@jsdevtools/npm-publish';
 import { fileURLToPath } from 'node:url';
+import { exec } from 'node:child_process';
 
-const getLastCommit = promisify(git.getLastCommit);
-const { body } = await getLastCommit();
-const isSemver = semverRegex().test(body);
+const e = promisify(exec);
+const commit = await e('git log -1 --pretty=%B', { encoding: 'utf-8' });
+const isSemver = semverRegex().test(commit.stdout);
 
-if (isSemver) await publish({ token: process.env.NPM_TOKEN, package: fileURLToPath(new URL('../package.json', import.meta.url)) });
-else console.log('No');
+if (isSemver) {
+	await publish({ token: process.env.NPM_TOKEN, package: fileURLToPath(new URL('../package.json', import.meta.url)) });
+	console.log('Published!');
+} else console.log('Not a release');
