6eab7e4c36cb14ac6c4d7165473492f516210a83
Tomio <mail@tomio.codes>
2022-03-29 18:51:07 +0800

    feat: init

--- lib/crypto/sign.ts
+++ lib/crypto/sign.ts
@@ -3,14 +3,14 @@ import type { KeyPair } from '../types';
 
 const sign = new Sign();
 
-export interface Hash {
+export interface SignHash {
 	state: BigUint64Array;
 	count: BigUint64Array;
 	buf: Uint8Array;
 }
 
-export interface State {
-	hs: Hash;
+export interface SignState {
+	hs: SignHash;
 }
 
 export const crypto_sign_BYTES: number = sign.cryptoSignBytes;
@@ -32,13 +32,13 @@ export const crypto_sign_ed25519_pk_to_curve25519 = (ed25519_pk: Uint8Array): Ui
 export const crypto_sign_ed25519_sk_to_curve25519 = (ed25519_sk: Uint8Array): Uint8Array => sign.crypto_sign_ed25519_sk_to_curve25519(ed25519_sk);
 
 /** @see https://docs.rs/libsodium-sys/latest/libsodium_sys/fn.crypto_sign_final_create.html */
-export const crypto_sign_final_create = (state: State, sk: Uint8Array): Uint8Array => sign.crypto_sign_final_create(state, sk);
+export const crypto_sign_final_create = (state: SignState, sk: Uint8Array): Uint8Array => sign.crypto_sign_final_create(state, sk);
 
 /** @see https://docs.rs/libsodium-sys/latest/libsodium_sys/fn.crypto_sign_final_verify.html */
-export const crypto_sign_final_verify = (state: State, sig: Uint8Array, pk: Uint8Array): boolean => sign.crypto_sign_final_verify(state, sig, pk);
+export const crypto_sign_final_verify = (state: SignState, sig: Uint8Array, pk: Uint8Array): boolean => sign.crypto_sign_final_verify(state, sig, pk);
 
 /** @see https://docs.rs/libsodium-sys/latest/libsodium_sys/fn.crypto_sign_init.html */
-export const crypto_sign_init = (): State => sign.crypto_sign_init();
+export const crypto_sign_init = (): SignState => sign.crypto_sign_init();
 
 /** @see https://docs.rs/sodiumoxide/latest/sodiumoxide/crypto/sign/ed25519/fn.gen_keypair.html */
 export const crypto_sign_keypair = (): KeyPair => sign.crypto_sign_keypair();
@@ -50,7 +50,7 @@ export const crypto_sign_open = (sm: Uint8Array, pk: Uint8Array): Uint8Array => 
 export const crypto_sign_seed_keypair = (seed: Uint8Array): KeyPair => sign.crypto_sign_seed_keypair(seed);
 
 /** @see https://docs.rs/libsodium-sys/latest/libsodium_sys/fn.crypto_sign_update.html */
-export const crypto_sign_update = (state: State, m: Uint8Array): State => sign.crypto_sign_update(state, m);
+export const crypto_sign_update = (state: SignState, m: Uint8Array): SignState => sign.crypto_sign_update(state, m);
 
 /** @see https://docs.rs/sodiumoxide/latest/sodiumoxide/crypto/sign/ed25519/fn.verify_detached.html */
 export const crypto_sign_verify_detached = (sig: Uint8Array, m: Uint8Array, pk: Uint8Array): boolean => sign.crypto_sign_verify_detached(sig, m, pk);
--- src/lib.rs
+++ src/lib.rs
@@ -7,3 +7,4 @@ pub mod rng;
 pub mod secretbox;
 pub mod sign;
 pub mod types;
+pub mod generichash;
